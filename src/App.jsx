639f87227f8a95b6932972ab13cc2e7dbb771b7b
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-14 05:24:51 +0200

    font config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl text-tofi">human rights have resulted</h1>
+    </>
+  );
 }
 
 export default App;
