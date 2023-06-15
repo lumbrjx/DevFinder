8063891d3f151c1628ab66d7a62c383584fad2b6
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-15 12:34:04 +0200

    svgr , folder structure , tailwind.config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-layoutClr">human rights have resulted</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
