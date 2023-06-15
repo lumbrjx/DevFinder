f41ea3832914b7a73431b4219120a9ac3aefbead
CLog <88345999+CLOG9@users.noreply.github.com>
2023-06-15 11:26:47 +0200

    setup tailwind.config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-tofi">human rights have resulted</h1>
+      <h1 className="text-3xl text-layoutClr">human rights have resulted</h1>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ export default {
   theme: {
     extend: {
       colors: {
-        tofi: "#ffffff",
+        primaryClr: "hsl(var(--primary-clr) / <alpha-value>)",
+        secondaryClr: "hsl(var(--secondary-cl) / <alpha-value>)",
+        layoutClr: "hsl(var(--layout-clr) / <alpha-value>)",
+        fontClr: "hsl(var(--font-clr) / <alpha-value>)",
+        disabledClr: "hsl(var(--disabled-clr) / <alpha-value>)",
+      },
+      fontSize: {
+        smallFont: "1rem",
+        largeFont: "2rem",
+      },
+      padding: {
+        generalPad: "0.5rem",
+        subPad: "0.3rem",
+      },
+      gap: {
+        generalGap: "0.5rem",
+        subGap: "0.3rem",
+      },
+      fontWeight: {
+        bold: "700",
+        normal: "400",
       },
     },
   },
